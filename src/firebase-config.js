98ab222cd870c2342a4d71a377332128b4cee4b2
Milena Souto <yamadasouto@gmail.com>
2023-01-23 17:17:43 -0800

    Set up firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+// config info
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_APIKEY,
+	authDomain: process.env.REACT_APP_AUTHDOMAIN,
+	projectId: process.env.REACT_APP_PROJECTID,
+	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+	appId: process.env.REACT_APP_APPID,
+	measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+// initialize app
+const app = initializeApp(firebaseConfig);
+
+// connect to database
+export const db = getFirestore();
