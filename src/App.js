6dccc0a9c0fa28d0403c2dd71d8e4daab23f3cb1
Milena Souto <yamadasouto@gmail.com>
2023-01-23 17:19:14 -0800

    Start App logic and create read bookclubs collection operation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { db } from "./firebase-config";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { async } from "@firebase/util";
 
 function App() {
-	return <div className="App"></div>;
+	// state for bookclubs collection
+	const [bookClubs, setBookClubs] = useState([]);
+	// create reference to bookclubs collections
+	const bookclubsCollectionRef = collection(db, "bookclubs");
+
+	// READ
+	// query when rendering page with useEffect
+	useEffect(() => {
+		const getBookClubs = async () => {
+			// API call to firestore db
+			const bookClubsData = await getDocs(bookclubsCollectionRef);
+
+			setBookClubs(
+				bookClubsData.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+			);
+		};
+		getBookClubs();
+	}, []);
+
+	return (
+		<div className="App">
+			{/* READ book club collections */}
+			{/* creating book club list components */}
+			{bookClubs.map((bookclub) => {
+				return (
+					<div>
+						<h2>Book Club: {bookclub.name} </h2>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
