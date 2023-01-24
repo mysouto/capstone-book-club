fff7290f0e23efb78ada72b2994d438ba6a2eacd
Milena Souto <yamadasouto@gmail.com>
2023-01-23 17:39:38 -0800

    Add create operation to add book club to bookclubs db collection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { db } from "./firebase-config";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { addDoc, collection, doc, getDocs } from "firebase/firestore";
 import { async } from "@firebase/util";
 
 function App() {
-	// state for bookclubs collection
-	const [bookClubs, setBookClubs] = useState([]);
 	// create reference to bookclubs collections
 	const bookclubsCollectionRef = collection(db, "bookclubs");
+
+	// state for bookclubs collection
+	const [bookClubs, setBookClubs] = useState([]);
+	// state to track add bookclub input
+	const [newBookClubName, setNewBookClub] = useState("");
 
 	// READ
 	// query when rendering page with useEffect
@@ -16,6 +19,7 @@ function App() {
 		const getBookClubs = async () => {
 			// API call to firestore db
 			const bookClubsData = await getDocs(bookclubsCollectionRef);
+			console.log(bookClubsData.docs);
 
 			setBookClubs(
 				bookClubsData.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
@@ -24,8 +28,28 @@ function App() {
 		getBookClubs();
 	}, []);
 
+	// CREATE
+	// add book club to "bookclubs" collection
+	const createBookClub = async () => {
+		// addDoc function, API call
+		await addDoc(
+			bookclubsCollectionRef,
+			// obj to add
+			{ name: newBookClubName }
+		);
+	};
+
 	return (
 		<div className="App">
+			{/* CREATE book club */}
+			<input
+				placeholder="Book Club Name..."
+				onChange={(event) => {
+					setNewBookClub(event.target.value);
+				}}
+			/>
+			<button onClick={createBookClub}>Create Book Club</button>
+
 			{/* READ book club collections */}
 			{/* creating book club list components */}
 			{bookClubs.map((bookclub) => {
