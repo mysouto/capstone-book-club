842d565608e22a5398f090c148bc0c9671d612e2
Milena Souto <yamadasouto@gmail.com>
2023-01-31 11:40:41 -0800

    seperate book club list and create book club features

--- src/pages/BookClubsPage.js
+++ src/pages/BookClubsPage.js
@@ -1,43 +1,16 @@
 import React from "react";
 
 import "../App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { app, db } from "../firebase-config";
-import {
-	addDoc,
-	collection,
-	doc,
-	deleteDoc,
-	getDocs,
-} from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 
 // Components
 import NewBookClubForm from "../components/NewBookClubForm";
-import BookClubList from "../components/BookClubList";
 
 function BookClubsPage() {
 	// create reference to bookclubs collections
 	const bookclubsCollectionRef = collection(db, "bookclubs");
-
-	// state for bookclubs db collection
-	const [bookClubs, setBookClubs] = useState([]);
-
-	// READ
-	// query when rendering page with useEffect
-	useEffect(() => {
-		const getBookClubs = async () => {
-			// API call to firestore db
-			const bookClubsRes = await getDocs(bookclubsCollectionRef);
-
-			setBookClubs(
-				bookClubsRes.docs.map((doc) => ({
-					...doc.data(),
-					bookClubID: doc.id,
-				}))
-			);
-		};
-		getBookClubs();
-	}, []);
 
 	// CREATE
 	// add book club to "bookclubs" collection
@@ -50,27 +23,10 @@ function BookClubsPage() {
 		);
 	};
 
-	// DELETE
-	const deleteBookClub = async (id) => {
-		console.log("calling deleteBookClub");
-		const bookClubDoc = doc(db, "bookclubs", id);
-		await deleteDoc(bookClubDoc);
-	};
-
 	return (
 		<div className="App">
 			<h1>Create a Book Club</h1>
-			<NewBookClubForm
-				createBookClub={createBookClub}
-				bookClubsData={bookClubs}
-			/>
-
-			<h2>Book Clubs List</h2>
-			{/* READ book club collections */}
-			<BookClubList
-				bookClubsData={bookClubs}
-				deleteBookClub={deleteBookClub}
-			/>
+			<NewBookClubForm createBookClub={createBookClub} />
 		</div>
 	);
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,61 @@
 import React from "react";
 
+import "../App.css";
+import { useState, useEffect } from "react";
+import { app, db } from "../firebase-config";
+import {
+	collection,
+	doc,
+	deleteDoc,
+	getDocs,
+} from "firebase/firestore";
+
+// Components
+import BookClubList from "../components/BookClubList";
+
 function Home() {
-	return <h1>THIS IS THE HOME PAGE</h1>;
+	// create reference to bookclubs collections
+	const bookclubsCollectionRef = collection(db, "bookclubs");
+
+	// state for bookclubs db collection
+	const [bookClubs, setBookClubs] = useState([]);
+
+	// READ
+	// query when rendering page with useEffect
+	useEffect(() => {
+		const getBookClubs = async () => {
+			// API call to firestore db
+			const bookClubsRes = await getDocs(bookclubsCollectionRef);
+
+			setBookClubs(
+				bookClubsRes.docs.map((doc) => ({
+					...doc.data(),
+					bookClubID: doc.id,
+				}))
+			);
+		};
+		getBookClubs();
+	}, []);
+
+	// DELETE
+	const deleteBookClub = async (id) => {
+		console.log("calling deleteBookClub");
+		const bookClubDoc = doc(db, "bookclubs", id);
+		await deleteDoc(bookClubDoc);
+	};
+
+	return (
+		<div className="App">
+			<h1>HOME PAGE</h1>
+
+			<h2>Book Clubs List</h2>
+			{/* READ book club collections */}
+			<BookClubList
+				bookClubsData={bookClubs}
+				deleteBookClub={deleteBookClub}
+			/>
+		</div>
+	);
 }
 
 export default Home;
