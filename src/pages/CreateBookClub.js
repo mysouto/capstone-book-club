431e205767989f3d0fca9dcb62e69c9724e73284
Milena Souto <yamadasouto@gmail.com>
2023-01-31 16:04:59 -0800

    Add navigate to book club page after creating book club feature

--- src/pages/CreateBookClub.js
+++ src/pages/CreateBookClub.js
@@ -1,7 +1,8 @@
-import React from "react";
-
 import "../App.css";
-import { useState } from "react";
+import React from "react";
+// import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { app, db } from "../firebase-config";
 import { addDoc, collection } from "firebase/firestore";
 
@@ -12,16 +13,34 @@ function CreateBookClub() {
 	// create reference to bookclubs collections
 	const bookclubsCollectionRef = collection(db, "bookclubs");
 
+	// let newBookClubID = "";
+	// const [newBookClubID, setID] = useState("");
+
+	let navigate = useNavigate();
+
 	// CREATE
 	// add book club to "bookclubs" collection
 	const createBookClub = async (bookClubName) => {
 		// addDoc function, API call
-		await addDoc(
+		const docRef = await addDoc(
 			bookclubsCollectionRef,
 			// obj to add
 			{ name: bookClubName }
 		);
+		// TODO - get book club id from firebase db response
+		// setID(docRef.id);
+		// newBookClubID = docRef.id;
+		// console.log(newBookClubID);
+
+		// Open Book Club home page
+		navigate(`/bookclubhome/${docRef.id}`);
 	};
+
+	// console.log("newBookClubID", newBookClubID);
+
+	// navigate(`/bookclubhome/${bookClubID}/searchbook`);
+	// OR
+	// navigate(`/bookclubhome/${bookClubID}`);
 
 	return (
 		<div className="App">
