bb89d6b23ddb3d2b7437429b1a0e5606662b8790
Milena Souto <yamadasouto@gmail.com>
2023-02-12 13:41:15 -0800

    style login page and add logout to nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 
 // React Router
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -13,13 +14,15 @@ import BookClubHome from "./pages/BookClubHome";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 
-import React from "react";
+import React, { useContext } from "react";
 // import UserContext
-import { UserContextProvider } from "./UserContext";
+import { UserContext, UserContextProvider } from "./UserContext";
 
-function App() {
+const Main = () => {
+	const { user, logout } = useContext(UserContext);
+
 	return (
-		<Router>
+		<>
 			<Navbar bg="dark" variant="dark">
 				<Container>
 					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -29,28 +32,42 @@ function App() {
 						<Nav.Link href="/createbookclub">
 							Create a Book Club
 						</Nav.Link>
-						<Nav.Link href="/signup">Sign Up</Nav.Link>
-						<Nav.Link href="/login">Login</Nav.Link>
+						{/* <Nav.Link href="/signup">Sign Up</Nav.Link> */}
+						{!user ? (
+							<Nav.Link href="/login">Login</Nav.Link>
+						) : (
+							<Button onClick={logout} variant="outline-success">
+								Logout
+							</Button>
+						)}
 					</Nav>
 				</Container>
 			</Navbar>
 
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route
+					path="/createbookclub"
+					element={<CreateBookClub />}
+				></Route>
+				{/* dynamic variable */}
+				<Route
+					path="/bookclubhome/:bookclubid"
+					element={<BookClubHome />}
+				></Route>
+				<Route path="/signup" element={<SignUp />}></Route>
+				<Route path="/login" element={<Login />}></Route>
+			</Routes>
+		</>
+	);
+};
+
+function App() {
+	return (
+		<Router>
 			{/* wrap components with Provider */}
 			<UserContextProvider>
-				<Routes>
-					<Route path="/" element={<Home />}></Route>
-					<Route
-						path="/createbookclub"
-						element={<CreateBookClub />}
-					></Route>
-					{/* dynamic variable */}
-					<Route
-						path="/bookclubhome/:bookclubid"
-						element={<BookClubHome />}
-					></Route>
-					<Route path="/signup" element={<SignUp />}></Route>
-					<Route path="/login" element={<Login />}></Route>
-				</Routes>
+				<Main />
 			</UserContextProvider>
 		</Router>
 	);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,13 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 
 import { UserContext } from "../UserContext";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+	let navigate = useNavigate();
 	const { user, logout } = useContext(UserContext);
 
 	const [loginEmail, setLoginEmail] = useState("");
@@ -14,15 +20,78 @@ function Login() {
 		try {
 			await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
 			console.log("logged in user: ", user);
+			navigate("/");
 		} catch (error) {
 			alert(error.message); // TODO: error handling on the frontend
 		}
 	};
 
 	return (
-		<div>
-			<h3> Login </h3>
-			<input
+		<div
+			style={{
+				margin: "0 auto",
+				padding: "0 10vw",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<h2> Login </h2>
+			<Form
+				className="p-3 bg-light"
+				style={{ height: "300px", width: "500px" }}
+			>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						type="email"
+						value={loginEmail}
+						placeholder="Email"
+						onChange={(event) => {
+							setLoginEmail(event.target.value);
+						}}
+					/>
+				</Form.Group>
+
+				<Form.Group className="mb-3" controlId="formBasicPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type="password"
+						value={loginPassword}
+						placeholder="Password"
+						onChange={(event) => {
+							setLoginPassword(event.target.value);
+						}}
+					/>
+				</Form.Group>
+
+				<pre>{JSON.stringify(user, null, 2)}</pre>
+				{user ? (
+					<Button onClick={logout} variant="primary" type="submit">
+						Log Out
+					</Button>
+				) : (
+					<Button onClick={login} variant="primary">
+						Log In
+					</Button>
+				)}
+			</Form>
+
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				<h2> Register </h2>
+				<h4>Not registered yet?</h4>
+				<Button onClick={() => navigate("/signup")}>Sign Up</Button>
+			</div>
+			{/* input version */}
+			{/* <input
 				value={loginEmail}
 				placeholder="Email..."
 				onChange={(event) => {
@@ -41,7 +110,7 @@ function Login() {
 				<button onClick={logout}>Log out</button>
 			) : (
 				<button onClick={login}>Log in</button>
-			)}
+			)} */}
 		</div>
 	);
 }
