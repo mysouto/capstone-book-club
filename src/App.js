c27d3a6f87a76be662e41e80973d00d057445fcd
Milena Souto <yamadasouto@gmail.com>
2023-02-13 16:12:07 -0800

    update Appcss and add hamburger to nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 // React Router
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -24,27 +25,61 @@ const Main = () => {
 	return (
 		<>
 			<Navbar
+				key="xl"
 				bg="light"
-				variant="light"
-				// bg-color="#2e9f9e" 
+				expand="lg"
+				className="mb-3"
+				// bg="light"
+				// variant="light"
+				// bg-color="#2e9f9e"
+				// className="bg-light expand navbar-expand-lg"
 			>
 				<Container>
 					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
-					<Nav>
-						<Nav.Link href="/">Home</Nav.Link>
-						{/* <Nav.Link href="/bookclubs">Book Clubs</Nav.Link> */}
-						<Nav.Link href="/createbookclub">
-							Create a Book Club
-						</Nav.Link>
-						{/* <Nav.Link href="/signup">Sign Up</Nav.Link> */}
-						{!user ? (
-							<Nav.Link href="/login">Login</Nav.Link>
-						) : (
-							<Button onClick={logout} variant="outline-success">
-								Logout
-							</Button>
-						)}
-					</Nav>
+					<Navbar.Toggle
+						aria-controls={"offcanvasNavbar-expand-lg"}
+					/>
+					<Navbar.Offcanvas
+						id={"offcanvasNavbar-expand-md"}
+						aria-labelledby={"offcanvasNavbarLabel-expand-lg"}
+						placement="end"
+					>
+						<Offcanvas.Header closeButton>
+							<Offcanvas.Title
+								id={"offcanvasNavbarLabel-expand-lg"}
+							>
+								Offcanvas
+							</Offcanvas.Title>
+						</Offcanvas.Header>
+						<Offcanvas.Body>
+							<Nav className="justify-content-end flex-grow-1 pe-3">
+								<Nav.Link href="/">Home</Nav.Link>
+								{/* <Nav.Link href="/bookclubs">Book Clubs</Nav.Link> */}
+								<Nav.Link href="/createbookclub">
+									Create a Book Club
+								</Nav.Link>
+								{/* <Nav.Link href="/signup">Sign Up</Nav.Link> */}
+								{!user ? (
+									<Nav.Link href="/login">Login</Nav.Link>
+								) : (
+									<Button
+										onClick={logout}
+										variant="outline-success"
+									>
+										Logout
+									</Button>
+								)}
+								{/* <button
+									aria-controls="offcanvasNavbar-expand-false"
+									type="button"
+									aria-label="Toggle navigation"
+									class="navbar-toggler collapsed"
+								>
+									<span class="navbar-toggler-icon"></span>
+								</button> */}
+							</Nav>
+						</Offcanvas.Body>
+					</Navbar.Offcanvas>
 				</Container>
 			</Navbar>
 
